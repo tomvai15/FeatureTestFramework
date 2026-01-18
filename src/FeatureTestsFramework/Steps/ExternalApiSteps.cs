56352287a3227c46ca8a1e48067fd16bc3c0518d
Tomas <Tomas@DESKTOP-RJ8A2OE>
2026-01-18 19:41:30 +0200

    fix

--- src/FeatureTestsFramework/Steps/ExternalApiSteps.cs
+++ src/FeatureTestsFramework/Steps/ExternalApiSteps.cs
@@ -24,6 +24,7 @@ public class ExternalApiSteps
     }
 
     [Given(@"service {string} api for {string} {string} returns")]
+    [Obsolete]
     public void ThenExternalApiForGetpostputdeletepatchheadoptionsStringReturns(string service, string method,
         string url,
         string content)
@@ -43,6 +44,44 @@ public class ExternalApiSteps
         _wireMockServer.Given(request).RespondWith(response);
     }
 
+    [Given(@"service {string} returns {int} for {string} {string} with body")]
+    public void GivenServiceStringReturnsIntForStringStringWithBody(string service, int statusCode, string method,
+        string url,
+        string content)
+    {
+        var trimmedUrl = url.TrimEnd('/').TrimStart('/');
+        var fullPath = $"/{service}/{trimmedUrl}";
+
+        var request = Request.Create()
+            .WithPath(fullPath)
+            .UsingMethod(method);
+
+        var response = Response.Create()
+            .WithStatusCode(statusCode)
+            .WithHeader("Content-Type", "application/json")
+            .WithBody(content);
+
+        _wireMockServer.Given(request).RespondWith(response);
+    }
+    
+    [Given(@"service {string} returns {int} for {string} {string}")]
+    public void GivenServiceStringReturnsIntForStringString(string service, int statusCode, string method,
+        string url)
+    {
+        var trimmedUrl = url.TrimEnd('/').TrimStart('/');
+        var fullPath = $"/{service}/{trimmedUrl}";
+
+        var request = Request.Create()
+            .WithPath(fullPath)
+            .UsingMethod(method);
+
+        var response = Response.Create()
+            .WithStatusCode(statusCode)
+            .WithHeader("Content-Type", "application/json");
+
+        _wireMockServer.Given(request).RespondWith(response);
+    }
+    
     [Then("service {string} was called with {string} {string}")]
     [Then("service {string} should be called with {string} {string}")]
     public void ThenServiceShouldBeCalled(string service, string method, string url, string body)
--- tests/FeatureTestFramework.Tests/Features/CreateLicenseGenerated.feature
+++ tests/FeatureTestFramework.Tests/Features/CreateLicenseGenerated.feature
@@ -1,4 +1,4 @@
-Feature: CreateLicense2
+Feature: CreateLicense
 
 Scenario: Scenario 1
 	Given I have an HTTP "POST" "PostLicense" request with body
@@ -9,20 +9,14 @@ Scenario: Scenario 1
 	  "userId": "A1SA5X"
 	}
 	"""
-	And service "FeatureFlagService" api for "POST" "PostFeatureFlags" returns
+	And service "FeatureFlagService" returns 200 for "POST" "PostFeatureFlags" with body
 	"""
 	{
 	  "featureFlag": "create_licenses_enabled",
 	  "isEnabled": "true"
 	}
 	"""
-	And service "LicenseBackendService" api for "POST" "PostNewLicense" returns
-	"""
-	{
-	  "productType": "VideoEditor",
-	  "userId": "A1SA5X"
-	}
-	"""
+	And service "LicenseBackendService" returns 200 for "POST" "PostNewLicense"
 	When I send the request
 	Then the response status code should be 200
 	And service "FeatureFlagService" was called with "POST" "PostFeatureFlags"
